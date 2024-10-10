1540ea66c8701065035210c2d7c71be726b9fa7a
Kristian <fich2402@gmail.com>
2024-10-10 16:28:33 +0200

    added theme update and changed profile

--- p_shell/profile.ps1
+++ p_shell/profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zash.custom.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\material.custom.omp.json" | Invoke-Expression
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
@@ -7,5 +7,5 @@ $env:PATH += ";$env:USERPROFILE\scoop\shims"
 Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
 
 function Touch {
-		    New-Item -Path $args[0] -ItemType "File" -Force
- }
+	New-Item -Path $args[0] -ItemType "File" -Force
+}
--- p_shell/update.ps1
+++ p_shell/update.ps1
@@ -1,13 +1,40 @@
-function Update  {
+function Update {
+	UpdateThemes
+	UpdateProfile
+}
+
+function UpdateProfile {
 	$sourceProfile = "$PSScriptRoot\profile.ps1"
 
 	$destinationProfile = $PROFILE
 
-	if(Test-Path $sourceProfile){
+	if (Test-Path $sourceProfile) {
 		Copy-Item -Path $sourceProfile -Destination $destinationProfile -Force
 		Write-Host "Profile updated"
-	}else{
+	}
+	else {
 		Write-Host "Source profile not found at $sourceProfile"
+	}
+}
+
+function UpdateThemes {
+	$themes = "$PSScriptRoot\custom_themes"
+
+	foreach ($theme in (Get-ChildItem $themes -Filter *.json)) {
+		$sourceTheme = "$themes\$($theme.Name)"
+		$destinationTheme = "$env:POSH_THEMES_PATH\$($theme.Name)"
+
+		try {
+			if (Test-Path $destinationTheme) {
+				Remove-Item -Path $destinationTheme -Force
+			}
+
+			Copy-Item -Path $sourceTheme -Destination $destinationTheme -Force
+			Write-Host "Theme $($theme.Name) updated"
+		}
+		catch {
+			Write-Host "Failed to update theme $($theme.Name): $_" -ForegroundColor Red
+		}
 	}
 }
 
